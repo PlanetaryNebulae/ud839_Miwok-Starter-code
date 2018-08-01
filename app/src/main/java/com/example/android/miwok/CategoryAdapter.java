87d7c9b4ae113a508a11f5c86596f44cea0525fd
PlanetaryNebulae <allsunnebula@gmail.com>
2018-07-31 23:47:09 -0700

    Yay! It's done!

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -1,14 +1,26 @@
 package com.example.android.miwok;
 
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
 public class CategoryAdapter extends FragmentPagerAdapter {
 
+    final int PAGE_COUNT = 4;
+
+    private String tabTitles[] = new String[]{"Numbers", "Family", "Colors", "Phrases"};
+    private Context context;
+
 
     public CategoryAdapter(FragmentManager fm) {
         super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
     }
 
     @Override
@@ -25,7 +37,7 @@ public class CategoryAdapter extends FragmentPagerAdapter {
     }
 
     @Override
-    public int getCount() {
-        return 4;
+    public CharSequence getPageTitle(int position) {
+        return tabTitles[position];
     }
 }
